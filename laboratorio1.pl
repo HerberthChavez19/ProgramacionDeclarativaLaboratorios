81f4bcc7dc46d2347bbbb013eb931b376caa3d7f
Herberth Alexander Ventura Chavez <00116223@uca.edu.sv>
2026-08-25 15:42:56 -0600

    added new rules

--- laboratorio1.pl
+++ laboratorio1.pl
@@ -25,14 +25,14 @@ zona(superficie).
 zona(cuevas).
 zona(bunkeres).
 
-% Enemigos y dónde aparecen
+% Enemigos y donde aparecen
 enemigo(canibales).
 enemigo(mutantes).
 aparece(canibales, superficie).
 aparece(mutantes, superficie).
 aparece(mutantes, cuevas).
 
-% Los búnkeres requieren llave para abrirse
+% Los bunkeres requieren llave para abrirse
 requiere_llave(bunkeres).
 
 % Nivel de peligro
@@ -45,26 +45,39 @@ necesita(eric, refugio).
 necesita(eric, comida).
 necesita(eric, agua).
 
-% Materiales y dónde se encuentran
+% Materiales y donde se encuentran
 material(troncos).
 material(piedras).
 se_encuentra(troncos, superficie).
 se_encuentra(piedras, superficie).
 
 % ---------------------- REGLAS ------------------------------
-
-% Regla 1: un personaje es adulto si su edad es >= 18.
+% Regla 1: una zona es peligrosa si aparece algun enemigo en ella o su nivel de peligro es alto.
+zona_peligrosa(Z) :-
+    zona(Z),
+    ( aparece(_, Z)
+    ; peligro(Z, alto)
+    ).
+ 
+% Regla 2: un personaje es adulto si su edad es mayor o igual a 18
 adulto(P) :-
     edad(P, E),
     E >= 18.
-
-% Regla 2: un personaje puede construir un refugio si sabe construir. Y hay troncos y piedras en una misma zona.
+ 
+% Regla 3: alguien es posible aliado si ya es aliado o es un mutante (puede volverse aliada).
+posible_aliado(P) :-
+    ( aliado(P)
+    ; mutante(P)
+    ).
+ 
+% Regla 4: un personaje puede construir un refugio si sabe construir. Y hay troncos y piedras en una misma zona.    (usa ,)
 puede_construir_refugio(P) :-
     puede_construir(P),
     se_encuentra(troncos, Z),
     se_encuentra(piedras, Z).
-
-% Regla 3: una zona es segura si no aparece ningún enemigo en ella. Los búnkeres cumplen, aunque requieran llave
+ 
+% Regla 5: una zona es segura si no aparece ningun enemigo en ella. Los bunkeres cumplen, aunque requieran llave
 zona_segura(Z) :-
     zona(Z),
     \+ aparece(_, Z).
+ 
